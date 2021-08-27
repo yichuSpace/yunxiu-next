502610456549bdba34521e8aa64f96ceff2efd60
奕初 <1244653559@qq.com>
2021-08-27 17:49:45 +0800

    feat: 🎸 add grid

---
+++ src/components/row/row.vue
@@ -0,0 +1,67 @@
+<template>
+  <div :class="classes" :style="styles">
+    <slot></slot>
+  </div>
+</template>
+<script>
+import { computed, provide } from 'vue'
+
+const prefixCls = 'yun-row'
+
+export default {
+  name: 'YunRow',
+  props: {
+    type: {
+      type: String,
+      validator: value => {
+        return ['flex'].includes(value)
+      },
+    },
+    align: {
+      type: String,
+      validator: value => {
+        return ['top', 'middle', 'bottom'].includes(value)
+      },
+    },
+    justify: {
+      type: String,
+      validator: value => {
+        return ['start', 'end', 'center', 'space-around', 'space-between'].includes(value)
+      },
+    },
+    gutter: {
+      type: Number,
+      default: 0,
+    },
+    className: String,
+  },
+  setup(props) {
+    provide('YunRow', props.gutter)
+    const classes = computed(() => {
+      return [
+        {
+          [`${prefixCls}`]: !props.type,
+          [`${prefixCls}-${props.type}`]: !!props.type,
+          [`${prefixCls}-${props.type}-${props.align}`]: !!props.align,
+          [`${prefixCls}-${props.type}-${props.justify}`]: !!props.justify,
+          [`${props.className}`]: !!props.className,
+        },
+      ]
+    })
+    const styles = computed(() => {
+      let style = {}
+      if (props.gutter !== 0) {
+        style = {
+          marginLeft: props.gutter / -2 + 'px',
+          marginRight: props.gutter / -2 + 'px',
+        }
+      }
+      return style
+    })
+    return {
+      classes,
+      styles,
+    }
+  },
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -6,12 +6,25 @@ import YunCountTo from './components/countTo'
 import YunInput from './components/input'
 import YunForm from './components/form'
 import YunFormItem from './components/form-item'
+import YunRow from './components/row'
+import YunCol from './components/col'
 
 import config from '../package.json'
 
 const version = config.version // version_ to fix tsc issue
 
-const components = [YunButton, YunIcon, YunAlert, YunMessage, YunCountTo, YunInput, YunForm, YunFormItem]
+const components = [
+  YunButton,
+  YunIcon,
+  YunAlert,
+  YunMessage,
+  YunCountTo,
+  YunInput,
+  YunForm,
+  YunFormItem,
+  YunRow,
+  YunCol,
+]
 
 const install = function(app, options = {}) {
   components.forEach(component => {
@@ -20,7 +33,7 @@ const install = function(app, options = {}) {
   return app
 }
 
-export { YunButton, YunIcon, YunAlert, YunMessage, YunCountTo, YunInput, YunForm, YunFormItem }
+export { YunButton, YunIcon, YunAlert, YunMessage, YunCountTo, YunInput, YunForm, YunFormItem, YunRow, YunCol }
 
 export default {
   version,
